de84dfc51e91115dbe1a6606a7698b769c1490d7
andredotio <87697887+andredotio@users.noreply.github.com>
2022-02-01 19:01:02 +0200

    Add solution for exercise 3.9

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,14 +1,15 @@
 const express = require('express')
 const app = express()
+const PORT = 3001
 
 const morgan = require('morgan')
 morgan.token('body', (req) => JSON.stringify(req.body))
 
-const PORT = 3001
+const cors = require('cors')
 
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
-
+app.use(cors())
 
 persons = [
     {
