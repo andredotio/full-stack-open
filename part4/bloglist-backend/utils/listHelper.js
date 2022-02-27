18378eaa27115c5f6f6be21b9bc38254a28c43e7
andredotio <87697887+andredotio@users.noreply.github.com>
2022-02-27 20:51:13 +0200

    Add solution for exercise 4.6

--- part4/bloglist-backend/utils/listHelper.js
+++ part4/bloglist-backend/utils/listHelper.js
@@ -40,8 +40,42 @@ const favouriteBlog = (blogs) => {
     return favBlog
 }
 
+const mostBlogs = (blogs) => {
+    if (blogs.length === 0) {
+        return {}
+    }
+
+    let index = 0;
+    let max = 0;
+
+    // returns an array of unique objects that conform to the following format: {author: someUniqueName, blogs: 0}
+    const authors = [...new Map(blogs.map((blog) => [blog["author"], { author: blog.author, blogs: 0 }])).values()]
+
+    for (let i = 0; i < authors.length; i++) {
+        for (let j = 0; j < blogs.length; j++) {
+            if (authors[i].author === blogs[j].author) {
+                authors[i].blogs++
+            }
+        }
+    }
+
+    for (let i = 0; i < authors.length; i++) {
+        if (authors[i].blogs > max) {
+            index = i
+        }
+    }
+
+    return authors[index]
+}
+
+const mostLikes = (blogs) => {
+    // todo
+}
+
 module.exports = {
     dummy,
     totalLikes,
-    favouriteBlog
+    favouriteBlog,
+    mostBlogs,
+    mostLikes
 }
