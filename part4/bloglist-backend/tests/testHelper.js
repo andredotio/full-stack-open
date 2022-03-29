2a869979ab80edf8df686045113dd8ff68ba3426
andredotio <87697887+andredotio@users.noreply.github.com>
2022-03-29 14:47:03 +0300

    Create a module with helper functions for testing the API

---
+++ part4/bloglist-backend/tests/testHelper.js
@@ -0,0 +1,45 @@
+const Blog = require('../models/blog')
+
+const initialBlogs = [
+    {
+        title: 'title1',
+        author: 'author1',
+        url: 'url1',
+        likes: 1
+    },
+    {
+        title: 'title2',
+        author: 'author2',
+        url: 'url2',
+        likes: 2
+    },
+    {
+        title: 'title3',
+        author: 'author3',
+        url: 'url3',
+        likes: 3
+    }
+]
+
+const blogsInDb = async () => {
+    const blogs = await Blog.find({})
+    return blogs.map(blog => blog.toJSON())
+}
+
+const nonExistingId = async () => {
+    const blog = new Blog({
+        title: 'title',
+        author: 'author',
+        url: 'url',
+        likes: 0
+    })
+
+    await blog.save()
+    await blog.remove()
+
+    return blog._id.toString()
+}
+
+module.exports = {
+    initialBlogs, blogsInDb, nonExistingId
+}
